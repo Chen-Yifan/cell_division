d444a7df96c98274ec1e929f1ade4deccd0bc564
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-10-20 04:25:43 -0500

    .

--- scripts/train.sh
+++
@@ -1,8 +0,0 @@
-python train.py \
---ckpt_path ./checkpoints/480_40cm_noresize_adad \
---results_path ./results/480_40cm_noresize_adad \
---network unet \
---epochs 40 \
---batch_size 16 \
---shape 224 \
---opt 0
--- scrpits/test.sh
+++ scrpits/test.sh
@@ -2,7 +2,7 @@ python prediction.py \
 --ckpt_path ./checkpoints/224random_unet_adadelta_50e \
 --results_path ./results/224random_unet_adadelta_50e \
 --network Unet \
---epochs 47 \
+--epochs 49 \
 --batch_size 1 \
 --opt 0 \
 --split test \
