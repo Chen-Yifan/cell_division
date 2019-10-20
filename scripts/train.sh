120b10ff7ea8c5a1e5135f427f72021f8dbfcbfb
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-10-20 06:52:18 -0500

    .

---
+++ scripts/train.sh
@@ -0,0 +1,9 @@
+python train.py \
+--ckpt_path ./checkpoints/224random_unet_adadelta_80e \
+--results_path ./results/224random_unet_adadelta_80e \
+--network unet \
+--epochs 80 \
+--batch_size 16 \
+--shape 224 \
+--opt 0
+
--- scrpits/train.sh
+++
@@ -1,9 +0,0 @@
-python train.py \
---ckpt_path ./checkpoints/224random_unet_adadelta_50e \
---results_path ./results/224random_unet_adadelta_50e \
---network unet \
---epochs 50 \
---batch_size 16 \
---shape 224 \
---opt 0
-
